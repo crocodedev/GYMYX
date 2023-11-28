9b70f4b25db5943f1d7476cb061abb1b5f00c7bc
Vadim <vadimm.crocode@gmail.com>
2023-11-28 13:41:36 +0300

    hero ready

---
+++ src/Components/Hero/index.jsx
@@ -0,0 +1,38 @@
+import Button from "../Button"
+import Container from "../Container"
+import styles from "./Hero.module.scss"
+
+const Hero = () => {
+  return (
+    <div className={styles.hero}>
+      <picture className={styles.hero__img}>
+        <source media="(max-width: 768px)" srcSet="/images/hero_mobile.png" />
+        <img src="/images/hero.png" alt="hero image" />
+      </picture>
+      <div className={styles["hero__content-wrapper"]}>
+        <Container>
+          <div className={styles.hero__content}>
+            <h1 className={styles.hero__title}>
+              Фитнес-студия с почасовой арендой
+            </h1>
+            <h2 className={styles.hero__subtitle}>
+              Занимайся спортом рядом с домом
+            </h2>
+            <div className={styles.hero__info}>
+              <p className={styles.hero__price}>
+                от
+                <span className={styles["hero__price-value"]}>
+                  590{" "}
+                  <span className={styles["hero__price-prefix"]}>₽/час</span>
+                </span>
+              </p>
+              <Button size="l" variant="black" label={"Записаться"} />
+            </div>
+          </div>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
