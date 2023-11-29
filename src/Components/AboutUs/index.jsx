8e22a7144744dfe11c0289739e228f7f68aa80c0
Vadim <vadimm.crocode@gmail.com>
2023-11-29 14:28:37 +0300

    section ready

---
+++ src/Components/AboutUs/index.jsx
@@ -0,0 +1,36 @@
+import BorderLabel from "../BorderLabel"
+import Container from "../Container"
+import styles from "./AboutUs.module.scss"
+import AboutUsItem from "./AboutUsItem"
+
+const LIST = [
+  { text: "Забронируйте удобный слот" },
+  { text: "Получите личный код для входа в студию" },
+  { text: "Занимайтесь одни или вместе с друзьями" },
+]
+
+const AboutUs = () => {
+  return (
+    <section className={styles["about-us"]}>
+      <Container>
+        <div className={styles["about-us__wrapper"]}>
+          <BorderLabel label={"Кто мы ?"} />
+          <h2 className={styles["about-us__title"]}>
+            GYMYX — это современная спортивная студия для индивидуальных
+            тренировок
+          </h2>
+          <div className={styles["about-us__content"]}>
+            <BorderLabel label={"как это работает?"} />
+            <div className={styles["about-us__list"]}>
+              {LIST.map(({ text }, index) => (
+                <AboutUsItem title={index + 1} text={text} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </Container>
+    </section>
+  )
+}
+
+export default AboutUs
