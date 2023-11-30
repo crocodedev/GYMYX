d59e6b5c2981c6b14fe8944187b96d72af12e126
Vadim <vadimm.crocode@gmail.com>
2023-11-30 15:33:16 +0300

    section ready

---
+++ src/Components/ChooseHealth/index.jsx
@@ -0,0 +1,20 @@
+import Container from "../Container"
+import styles from "./ChooseHealth.module.scss"
+
+const ChooseHealth = () => {
+  return (
+    <section className={styles["choose-health"]}>
+      <Container>
+        <picture className={styles["choose-health__image"]}>
+          <source
+            media="(max-width: 992px)"
+            srcSet="/images/chooseHealth-mobile.png"
+          />
+          <img src="/images/chooseHealth.png" alt="choose health image" />
+        </picture>
+      </Container>
+    </section>
+  )
+}
+
+export default ChooseHealth
