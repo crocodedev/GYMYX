18e1e6def857ae39470d19bb73bfa441fe1eb18a
Vadim <vadimm.crocode@gmail.com>
2023-12-25 12:35:20 +0300

    added pwa

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const path = require("path")
+const withPWA = require("next-pwa")
 
 const nextConfig = {
+  ...withPWA({
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  }),
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
