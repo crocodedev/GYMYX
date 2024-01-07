031aec7e4fcd4b33ba7ce21572bf88b2cd16db7f
Vadim <vadimm.crocode@gmail.com>
2024-01-07 14:50:12 +0300

    update

--- src/Components/Checkout/CheckoutList/index.jsx
+++ src/Components/Checkout/CheckoutList/index.jsx
@@ -1,44 +1,48 @@
-import styles from "./CheckoutList.module.scss"
+import styles from './CheckoutList.module.scss';
 
-import { useSelector, useDispatch } from "react-redux"
-import { updateBookingData } from "@/redux/bookingSlice"
+import { useSelector, useDispatch } from 'react-redux';
+import { updateBookingData } from '@/redux/bookingSlice';
 
-import { sortVisitDates, canDelete } from "./helpers"
-import BookingCard from "@/Components/Booking/BookingCard"
-import { useSession } from "next-auth/react"
+import { sortVisitDates, canDelete } from './helpers';
+import BookingCard from '@/Components/Booking/BookingCard';
+import { useSession } from 'next-auth/react';
 
 const CheckoutList = ({ items }) => {
-  const dispatch = useDispatch()
-  const { gym, visitDate } = useSelector((state) => state.booking)
-  const { data: sessionData } = useSession()
-  const handleDeleteItem = (valueDate, valueTime) => {
+  const dispatch = useDispatch();
+  const { gym, visitDate } = useSelector((state) => state.booking);
+  const { data: sessionData } = useSession();
+
+  const handleDeleteItem = (data) => {
+    console.log("asdasd",canDelete(items))
     if (canDelete(items)) {
-      const data = sortVisitDates(visitDate, valueDate, valueTime)
-      dispatch(updateBookingData({ visitDate: data, currentDate: 0 }))
+      const { valueDate, valueTime } = data;
+      const newVisitDates = sortVisitDates(visitDate, valueDate, valueTime);
+      dispatch(updateBookingData({ visitDate: newVisitDates, currentDate: 0 }));
     }
-  }
+  };
 
   return (
-    <div className={styles["checkout-list"]}>
-      <div className={styles["checkout-list__wrapper"]}>
+    <div className={styles['checkout-list']}>
+      <div className={styles['checkout-list__wrapper']}>
         {items.map(({ time, value }, index) => {
           return time.map((timeZome) => {
             return (
               <BookingCard
-                isSingle={timeZome.length === 1 && items.length === 1}
-                key={index}
+                id={{ valueDate: value, valueTime: timeZome }}
+                isSingle={!canDelete(items)}
+                key={`checkout_card_${index}`}
                 onClickDelete={handleDeleteItem}
                 date={value}
                 time={timeZome}
                 gymTitle={gym?.name}
                 address={gym?.address}
               />
-            )
-          })
+            );
+          });
         })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default CheckoutList
+export default CheckoutList;
--- src/app/account/booking/sign-up/choose-time/page.jsx
+++ src/app/account/booking/sign-up/choose-time/page.jsx
@@ -1,79 +1,79 @@
-"use client"
+'use client';
 
-import BookingSignUpHeading from "@/Sections/Account/Booking/BookingSignUpHeading"
-import BookingSignUpTags from "@/Sections/Account/Booking/BookingSignUpTags"
-import BookingSignUpContent from "@/Sections/Account/Booking/BookingSignUpContent"
-import BookingSteps from "@/Sections/Account/Booking/BookingSteps"
-import NavigationBack from "@/Sections/Account/NavigationBack"
-import BookingTimePricing from "@/Sections/Account/Booking/BookingTimePricing"
+import BookingSignUpHeading from '@/Sections/Account/Booking/BookingSignUpHeading';
+import BookingSignUpTags from '@/Sections/Account/Booking/BookingSignUpTags';
+import BookingSignUpContent from '@/Sections/Account/Booking/BookingSignUpContent';
+import BookingSteps from '@/Sections/Account/Booking/BookingSteps';
+import NavigationBack from '@/Sections/Account/NavigationBack';
+import BookingTimePricing from '@/Sections/Account/Booking/BookingTimePricing';
 
-import { useDispatch, useSelector } from "react-redux"
-import { useEffect, useState } from "react"
-import { useSession } from "next-auth/react"
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useSession } from 'next-auth/react';
 
 const variants = [
-  { value: "00:00", bgColor: "#7B92FF" },
-  { value: "08:00", bgColor: "#294AE7" },
-  { value: "13:00", bgColor: "#1E318A" },
-  { value: "19:00", bgColor: "#061641" },
-]
+  { value: '00:00', bgColor: '#7B92FF' },
+  { value: '08:00', bgColor: '#294AE7' },
+  { value: '13:00', bgColor: '#1E318A' },
+  { value: '19:00', bgColor: '#061641' },
+];
 
 const checkIsOnlyTraining = (data) => {
   let result = true;
-  data.forEach(({time})=>{
-    if(!!time.length) {
+  data.forEach(({ time }) => {
+    if (!!time.length) {
       result = false;
       return;
     }
-  })
+  });
 
   return result;
-}
+};
 
 const ChooseTime = () => {
-  const { data: sessionData } = useSession()
-  const { gym, visitDate } = useSelector((state) => state.booking)
-  const [pricesVariants, setPricesVariants] = useState([])
+  const { data: sessionData } = useSession();
+  const { gym, visitDate } = useSelector((state) => state.booking);
+  const [pricesVariants, setPricesVariants] = useState([]);
 
   useEffect(() => {
     if (gym?.prices && sessionData) {
-      let variantsTemp = []
-      if (sessionData.user.enter_code && !checkIsOnlyTraining(visitDate) || !checkIsOnlyTraining(visitDate)) {
-        variantsTemp = gym.prices.map((item, index) => {
-          return { ...item, bgColor: variants[index]?.bgColor }
-        })
-      } else {
-        variantsTemp = [
-          {
-            start: "00:00:00",
-            end: "23:00:00",
-            price: gym?.min_price,
-            bgColor: variants[0]?.bgColor,
-          },
-        ]
-      }
+      let variantsTemp = [];
 
-      setPricesVariants(variantsTemp)
+      if (sessionData.user.enter_code) {
+        variantsTemp = gym.prices.map((item, index) => {
+          return { ...item, bgColor: variants[index]?.bgColor };
+        });
+      } else {
+        if (!checkIsOnlyTraining(visitDate)) {
+          variantsTemp = gym.prices.map((item, index) => {
+            return { ...item, bgColor: variants[index]?.bgColor };
+          });
+        } else {
+          variantsTemp = [
+            {
+              start: '00:00:00',
+              end: '23:00:00',
+              price: gym?.min_price,
+              bgColor: variants[0]?.bgColor,
+            },
+          ];
+        }
+      }
+      setPricesVariants(variantsTemp);
     }
-  }, [gym, sessionData, visitDate])
+  }, [gym, sessionData, visitDate]);
 
   return (
     <>
-      <NavigationBack
-        buttonLabel={"Вернуться к выбору дней"}
-        link={"/account/booking/sign-up"}
-      />
-      <BookingSignUpHeading
-        showButtonEditGym={false}
-        headingTitle={"Купить несколько тренировок"}
-      />
+      <NavigationBack buttonLabel={'Вернуться к выбору дней'} link={'/account/booking/sign-up'} />
+      <BookingSignUpHeading showButtonEditGym={false} headingTitle={'Купить несколько тренировок'} />
       <BookingSignUpTags />
       <BookingSignUpContent>
         <BookingTimePricing variants={pricesVariants} />
-        <BookingSteps stepNumber={2} stepTitle={"Выберите время"} />
+        <BookingSteps stepNumber={2} stepTitle={'Выберите время'} />
       </BookingSignUpContent>
     </>
-  )
-}
+  );
+};
 
-export default ChooseTime
+export default ChooseTime;
