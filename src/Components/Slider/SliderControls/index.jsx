40582ef3ed280faa630f52ea91a9a77a6965590a
Daniil Yurkevich <yurkevich.crocode@gmail.com>
2024-02-19 02:28:13 +0300

    little fix

--- src/Components/Slider/SliderControls/index.jsx
+++ src/Components/Slider/SliderControls/index.jsx
@@ -10,14 +10,14 @@ const SliderControls = ({ handlePrevSlide, handleNextSlide, activeSlide, countSl
       </p>
       <div className={styles['slider-controls__btns']}>
         <button onClick={handlePrevSlide} className={styles['slider-controls__btn']} aria-label="Предыдущий слайд">
-          <img src="/icons/arrow.svg" alt="" />
+          <img src="/icons/arrow.svg" alt="arrow-prev" width={30} height={30} />
         </button>
         <button
           onClick={handleNextSlide}
           className={`${styles['slider-controls__btn']} ${styles['slider-controls__btn-right']}`}
           aria-label="Следующий слайд"
         >
-          <img src="/icons/arrow.svg" alt="" />
+          <img src="/icons/arrow.svg" alt="arrow-next" width={30} height={30} />
         </button>
       </div>
     </div>
