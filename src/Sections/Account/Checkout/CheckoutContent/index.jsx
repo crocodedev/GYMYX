f1b383840fd3ca56d277bd1827ee507f7228d186
yurkevich-crocode <yurkevich.crocode@gmail.com>
2024-05-17 13:01:40 +0300

    before test

--- src/Sections/Account/Checkout/CheckoutContent/index.jsx
+++ src/Sections/Account/Checkout/CheckoutContent/index.jsx
@@ -1,28 +1,30 @@
-"use client"
+'use client';
 
-import { useSelector } from "react-redux"
+import { useSelector } from 'react-redux';
 
-import CheckoutList from "@/Components/Checkout/CheckoutList"
-import CheckoutSummary from "@/Components/Checkout/CheckoutSummary"
-import Container from "@/Components/Container"
+import CheckoutList from '@/Components/Checkout/CheckoutList';
+import CheckoutSummary from '@/Components/Checkout/CheckoutSummary';
+import Container from '@/Components/Container';
 
-import styles from "./CheckoutContent.module.scss"
-import Modal from "@/Components/Modal"
-import { useState } from "react"
+import styles from './CheckoutContent.module.scss';
+import Modal from '@/Components/Modal';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 
 const CheckoutContent = () => {
-  const { visitDate, gym } = useSelector((state) => state.booking)
+  const router = useRouter();
+  const { visitDate, gym } = useSelector((state) => state.booking);
 
   return (
-    <div className={styles["checkout-content"]}>
+    <div className={styles['checkout-content']}>
       <Container>
-        <div className={styles["checkout-content__wrapper"]}>
+        <div className={styles['checkout-content__wrapper']}>
           <CheckoutList items={visitDate} />
           <CheckoutSummary items={visitDate} gym={gym} />
         </div>
       </Container>
     </div>
-  )
-}
+  );
+};
 
-export default CheckoutContent
+export default CheckoutContent;
