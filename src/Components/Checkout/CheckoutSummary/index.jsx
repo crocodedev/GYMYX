a0fd9d1d05e70434dd9e1ec74758eda2c1b45c1c
yurkevich-crocode <yurkevich.crocode@gmail.com>
2024-10-09 14:24:43 +0300

    fix correct date payment

--- src/Components/Checkout/CheckoutSummary/index.jsx
+++ src/Components/Checkout/CheckoutSummary/index.jsx
@@ -57,7 +57,7 @@ const CheckoutSummary = ({ items, gym }) => {
 
   const handleSubmit = () => {
     setLoading(true);
-    createBooking(sessionData.user.accessToken, gym?.id, prepareDataForBooking(finalArr)).then(({ data }) => {
+    createBooking(sessionData.user.accessToken, gym?.id, prepareDataForBooking(list)).then(({ data }) => {
       if (data?.payment_link) {
         router.push(data?.payment_link);
       } else if (data?.status) {
