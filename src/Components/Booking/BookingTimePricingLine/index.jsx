9673fddba706b63e236b0e77dbb96ae808ab909c
yurkevich-crocode <yurkevich.crocode@gmail.com>
2025-01-14 17:05:36 +0300

    change shadow in studio guide

--- src/Components/Booking/BookingTimePricingLine/index.jsx
+++ src/Components/Booking/BookingTimePricingLine/index.jsx
@@ -7,10 +7,8 @@ import { useSelector } from "react-redux";
 const BookingTimePricingLine = ({ variants, priceVariant, isChange = false }) => {
   const { visitDate, gym } = useSelector((state) => state.booking)
   const dateLength = visitDate.reduce((acc, el) => acc + el.time.length, 0)
-  console.log(gym.prices)
-  console.log(priceVariant)
 
-  const arr = [
+  const variantPrices = [
     {name: 'Ранее утро', price: gym.prices[0].price, color: '#7B92FF'},
     {name: 'Утро', price: gym.prices[1].price, color: '#294AE7'},
     {name: 'День', price: gym.prices[2].price, color: '#1E318A'},
@@ -36,9 +34,9 @@ const BookingTimePricingLine = ({ variants, priceVariant, isChange = false }) =>
             value={price}
           />
         ))
-      ) : (
+      ) : !dateLength && (
         !dateLength ? (
-          updatePrices(arr, priceVariant.first).map(({price, color}) => (
+          updatePrices(variantPrices, priceVariant.first).map(({price, color}) => (
             <BookingTimePricingLineItem
               key={price}
               bgColor={color}
@@ -46,7 +44,7 @@ const BookingTimePricingLine = ({ variants, priceVariant, isChange = false }) =>
             />
           ))
         ) : (
-          updatePrices(arr, priceVariant.default).map(({price, color}) => (
+          updatePrices(variantPrices, priceVariant.default).map(({price, color}) => (
             <BookingTimePricingLineItem
               key={price}
               bgColor={color}
